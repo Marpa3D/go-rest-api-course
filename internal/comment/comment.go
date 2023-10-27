c489fe36b176b903dff2db727f0b818b571bffe5
Viacheslav "Marpa3D" Sharov <Marpa3d@yandex.ru>
2023-10-27 07:56:45 +0300

    Создал структуру для комментариев и конструктор для нового сервиса

--- internal/comment/comment.go
+++ internal/comment/comment.go
@@ -1,1 +1,17 @@
 package comment
+
+// Comment - предсатвление структуры комментариев для нашего сервиса
+type Comment struct {
+	ID     string
+	Slug   string
+	Body   string
+	Author string
+}
+
+type Service {}
+
+// NewService - конструктор нового сервиса.
+// Возвращает указатель на новый сервис
+func NewService() *Service {
+	return &Service
+}
